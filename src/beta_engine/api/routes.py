b52f0bd0f8c3eaf07b183318e61d3d92032b83e0
Jasmetk0 <50439468+Jasmetk0@users.noreply.github.com>
2026-09-20 15:35:19 +0200

    Register prospect visibility routes

--- src/beta_engine/api/routes.py
+++ src/beta_engine/api/routes.py
@@ -43,6 +43,7 @@ from beta_engine.api.routers.world_manual_player_overrides import router as worl
 from beta_engine.api.routers.world_package import router as world_package_router
 from beta_engine.api.routers.world_packages import router as world_packages_router
 from beta_engine.api.routers.viewer_run_context import router as viewer_run_context_router
+from beta_engine.api.routers.prospects import router as prospects_router
 
 router = APIRouter()
 router.include_router(admin_authoritative_simulation_router)
@@ -85,3 +86,4 @@ router.include_router(world_manual_player_overrides_router)
 router.include_router(world_package_router)
 router.include_router(world_packages_router)
 router.include_router(viewer_run_context_router)
+router.include_router(prospects_router)

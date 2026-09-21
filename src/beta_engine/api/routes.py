0719760133f0287a957e2d61ed01e8001e02af56
Jasmetk0 <50439468+Jasmetk0@users.noreply.github.com>
2026-09-21 09:50:04 +0200

    Register canonical WC Admin routes

--- src/beta_engine/api/routes.py
+++ src/beta_engine/api/routes.py
@@ -6,6 +6,7 @@ from beta_engine.api.routers.admin_ranking_candidates import router as admin_ran
 from beta_engine.api.routers.admin_week_transitions import router as admin_week_transitions_router
 from beta_engine.api.routers.admin_authoritative_simulation import router as admin_authoritative_simulation_router
 from beta_engine.api.routers.admin_tournament_entry_fields import router as admin_tournament_entry_fields_router
+from beta_engine.api.routers.admin_tournament_wild_cards import router as admin_tournament_wild_cards_router
 from beta_engine.api.routers.admin_tournament_draw_authority import router as admin_tournament_draw_authority_router
 from beta_engine.api.routers.admin_tournament_draw_process import router as admin_tournament_draw_process_router
 from beta_engine.api.routers.admin_player_prize_money import router as admin_player_prize_money_router
@@ -48,6 +49,7 @@ from beta_engine.api.routers.prospects import router as prospects_router
 router = APIRouter()
 router.include_router(admin_authoritative_simulation_router)
 router.include_router(admin_tournament_entry_fields_router)
+router.include_router(admin_tournament_wild_cards_router)
 router.include_router(admin_tournament_draw_authority_router)
 router.include_router(admin_tournament_draw_process_router)
 router.include_router(admin_player_prize_money_router)

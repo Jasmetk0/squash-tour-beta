9fa44db37e11018ea72e4699910830d22f77e660
Jasmetk0 <50439468+Jasmetk0@users.noreply.github.com>
2026-09-26 13:52:44 +0200

    Use shared legacy API test templates

--- tests/api/test_admin_weeks_recovery_api.py
+++ tests/api/test_admin_weeks_recovery_api.py
@@ -4,7 +4,7 @@ import json
 
 import test_admin_lifecycle_api as lifecycle_api
 from test_admin_lifecycle_api import Server, call
-from test_admin_weeks_run_api import write_complete_templates
+from legacy_season_api_test_support import write_complete_templates
 
 
 def test_post_week_recovery_no_calendar(tmp_path):

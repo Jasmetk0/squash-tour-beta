52174d4ce84c3a6cd461a404c66dc551fdda3c7d
Jasmetk0 <50439468+Jasmetk0@users.noreply.github.com>
2026-09-26 13:54:05 +0200

    Use shared legacy API test templates

--- tests/api/test_admin_seasons_range_preflight_api.py
+++ tests/api/test_admin_seasons_range_preflight_api.py
@@ -7,7 +7,7 @@ from urllib.error import HTTPError
 
 import test_admin_lifecycle_api as lifecycle_api
 from test_admin_lifecycle_api import Server, call
-from test_admin_weeks_run_api import write_complete_templates
+from legacy_season_api_test_support import write_complete_templates
 
 
 @pytest.mark.pr_critical
